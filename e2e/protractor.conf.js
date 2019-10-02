425c927215be9cf9adac860784ec9c0226a98589
mgrabus <maciej.grabus@softiq.pl>
2019-10-02 14:05:55 +0200

    Po testach

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,9 +12,17 @@ exports.config = {
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
+  capabilities: 
+  {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+        "--headless"
+      ]
+    }
   },
+
+
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('facebook app is running!');
+    expect(page.getTitleText()).toEqual('FACEBOOK');
   });
 
   afterEach(async () => {
