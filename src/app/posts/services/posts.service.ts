f6bc82ad78cf0275196c86c455b729c6d82dbc38
mgrabus <maciej.grabus@softiq.pl>
2019-10-03 14:36:38 +0200

    Dzien 2 - 4

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { IPostList } from 'src/app/shared/interfaces/post-list.interface';
 import { post } from 'selenium-webdriver/http';
 import { sortByOperator } from 'src/app/shared/helpers/sorter.helper';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,7 @@ export class PostsService {
   }
 
   async getPosts() {
-    const url = 'assets/fake-posts.json';
+    const url = environment.postsUrl;
     const response = await this.http.get<IPostList>(url)
     .pipe(
       sortByOperator('createdTime')
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  postsUrl: 'https://fakes.herokuapp.com/posts'
 };
